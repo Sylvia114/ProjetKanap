725e291ad117e421da99b978497b8e80ca6c0ac1
Sylvia M <mangomanguee@gmail.com>
2022-11-17 20:06:07 +0100

    Correction de l'event click pour ajout & de  la boucle recap panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,30 +1,97 @@
-let basket = JSON.parse(localStorage.getItem("product"))
-console.log(basket);
-//console.log(getBasket)
+basket = JSON.parse(localStorage.getItem("product"))
+//console.log(basket)
 
-function products(basket){
-  let baskettest = [{"productId":"107fb5b75607497b96722bda5b504926","productQuantity":"3","productColor":"Blue"}]
-  const section = document.getElementById("cart__items")
 
- // for (let i = 0; i < basket.length; i++) {
+for (let i = 0; i < basket.length; i++) {
 
+    let section = document.getElementById("cart__items")
+        
+//ajouter un if pour ne pas ajouter pls fois le même article (même id + même couleur),
+
+    // 1e niveau Container article
     const article = document.createElement("article")
     section.appendChild(article)
     article.classList.add("cart__item")
-    
-    const divImg = document.createElement("div")
-    article.appendChild(divImg)
+    article.setAttribute("data-id", "{product-ID}")
+    article.setAttribute("data-color", "{product-color}")
 
-    const productPicture = document.createElement("img")
-    divImg.appendChild(productPicture)
-    //productPicture.setAttribute("src", basket[i].imageUrl) 
-    //productPicture.setAttribute("alt", basket[i].altTxt)
-    console.log(article)
+      // 2e niveau 1_Image
+        const newDivImg = document.createElement('div')
+        newDivImg.classList.add("cart__item__img")
+        //console.log(newDivImg)
 
-  //}
+        const newImg = document.createElement('img')
+        newDivImg.appendChild(newImg)
+        newImg.setAttribute("src", basket[i].imageUrl)
+        newImg.setAttribute("alt", basket[i].altTxt)
+        //console.log(newImg)
+
+        // 2e niveau 2_Container éléments contenus dans le panier
+        const newDivItemContent = document.createElement('div')
+        newDivItemContent.classList.add("cart__item__content")
+        //console.log(newDivItemContent)
+
+            // 3e niveau 1_Eléments contenus dans le panier - description
+            const newDivItemDescription = document.createElement('div')
+            newDivItemContent.appendChild(newDivItemDescription)
+            newDivItemDescription.classList.add("cart__item__content__description")
+            //console.log(newDivItemDescription)
+
+            const newH2name = document.createElement('h2')
+            newDivItemDescription.appendChild(newH2name)
+            newH2name.innerHTML = basket[i].name
+            //console.log(newH2name)
+
+            const newPcolor = document.createElement('p')
+            newDivItemDescription.appendChild(newPcolor)
+            newPcolor.innerHTML = basket[i].colors
+            //console.log(newPcolor)
+
+            const newPprice = document.createElement('p')
+            newDivItemDescription.appendChild(newPprice)
+            newPprice.innerHTML = basket[i].price
+            //console.log(newPcolor)
+
+            // 3e niveau 2_Eléments contenus dans le panier - settings
+            const newDivItemSettings = document.createElement('div')
+            newDivItemContent.appendChild(newDivItemSettings)
+            newDivItemSettings.classList.add("cart__item__content__settings")
+            //console.log(newDivItemSettings)
+
+                // 4e niveau 1_Quantité
+                const newDivItemQuantity = document.createElement('div')
+                newDivItemSettings.appendChild(newDivItemQuantity)
+                newDivItemQuantity.classList.add("cart__item__content__settings__quantity")
+                newDivItemQuantity.innerHTML = basket[i].quantity
+
+                const newPquantity = document.createElement('p')
+                newDivItemQuantity.appendChild(newPquantity)
+                newPquantity.innerHTML = basket[i].quantity
+                //console.log(newPquantity)
+
+                const newInput = document.createElement('input')
+                newDivItemQuantity.appendChild(newInput)
+                newInput.setAttribute("type", "number")
+                newInput.classList.add("itemQuantity")
+                newInput.setAttribute("name", "itemQuantity")
+                newInput.setAttribute("min", "1")
+                newInput.setAttribute("max", "100")
+                newInput.setAttribute("value", "42")
+
+                // 4e niveau 2_Supprimer
+                const newDivItemDelete = document.createElement('div')
+                newDivItemSettings.appendChild(newDivItemDelete)
+                newDivItemDelete.classList.add("cart__item__content__settings__delete")
+
+                const newPdelete = document.createElement('p')
+                newDivItemDelete.appendChild(newPdelete)
+                newPdelete.classList.add("deleteItem")
+                //newPdelete.innerHTML("supprimer")
+
+        article.appendChild(newDivImg)
+        article.appendChild(newDivItemContent)
+        //console.log(article)
+        console.log(section)
 
 };
-
-
-  
 
--- front/js/product.js
+++ front/js/product.js
@@ -40,44 +40,57 @@ function products(data){
     productContainer.setAttribute("value", colors[i])
     productContainer.innerHTML = colors[i]
     productColor.appendChild(productContainer)
-    }
+    };
 
 }; 
-
 
 // Ajouter des produits dans le panier
 
 const button = document.querySelector ("#addToCart")
-button.addEventListener("click", function() {
+button.addEventListener("click", function(click) {
+click.preventDefault();
 
-let productOptions = {
-  id : urlId,
-  quantity : document.querySelector("#quantity").value,
-  color : document.querySelector("#colors").value,
-}
+  let productOptions = {
+    id : urlId,
+    quantity : document.querySelector("#quantity").value,
+    colors : document.querySelector("#colors").value,
+    name : document.getElementById("title").textContent,
+    description : document.getElementById("description").textContent,
+    imageUrl : document.querySelector(".item__img img").src,
+    altTxt : document.querySelector(".item__img img").alt,
+    price : document.querySelector(".item__content__titlePrice p span").textContent,
+  };
 
-let basketLocal = JSON.parse(localStorage.getItem("product"))
-
-if (basketLocal == null){
-  let basketLocal = [];
-  basketLocal.push(productOptions);
-  localStorage.setItem("product", JSON.stringify(basketLocal));
-  console.log(basketLocal)
-} else {
-  let i = 0;
-    while (i < localStorage.length){
-    i++;    {  
-    if (productOptions.id==urlId && productOptions.color==colors.value){ //Corriger les conditions pour que ce soit currentId==localStorageId et non pas currentId==currentId
-      let localQuantity = basketLocal.quantity;
-      localQuantity = parseInt(localQuantity) + parseInt(quantity.value);
-      localStorage.setItem("product", JSON.stringify(basketLocal)); //Chercher comment push la nouvelle valeur quantité dans le local storage
-      console.log(localQuantity)
-    } else {
-        basketLocal.push(productOptions);
-        localStorage.setItem("product", JSON.stringify(basketLocal));
+  //stockInLocal = localStorage.setItem("product", JSON.stringify(productOptions))
+  getFromLocal = JSON.parse(localStorage.getItem("product", productOptions))
+  
+  if (getFromLocal == null){
+    let getFromLocal = [];
+    getFromLocal.push(productOptions);
+    localStorage.setItem("product", JSON.stringify(productOptions));
+    console.log(getFromLocal)
+  } else {
+    let i = 0;
+      while (i < localStorage.length){
+      i++;   {
+        if(getFromLocal.id == urlId) {
+          let localQuantity = getFromLocal.quantity
+          localQuantity = parseInt(localQuantity) + parseInt(productOptions.quantity)
+          console.log("ça marche"+ localQuantity)
+          //push la quantity seulement
+        } else {
+          console.log("ça marche pas vraiment")
+        }
+      }
+  
     }
   }
+  });
 
-  }
-}
-});
+
+  //let from = getFromLocal.quantity la quantité dans le local
+  //let inU = productOptions.quantity la quantité sur laquelle on a cliqué
+  //console.log("ça marche"+from+"&"+inU)
+
+  //let colIn = getFromLocal.colors couleur dans le local
+  //let colCh = productOptions.colors couleur sur laquelle on clique
