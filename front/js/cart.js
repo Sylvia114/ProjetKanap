2f36b060655f78cc2c81e79199b4a30c71ace96e
Sylvia M <mangomanguee@gmail.com>
2022-12-07 10:51:20 +0100

    Correction regex

--- front/js/cart.js
+++ front/js/cart.js
@@ -173,97 +173,89 @@ for (let i = 0; i < basket.length; i++){
 let firstName = document.querySelector("#firstName");
 let firstNameErrorMsg = document.querySelector("#firstNameErrorMsg");
 firstName.addEventListener("change", function(){
-    checkFirstName(this);
-});
-let checkFirstName = function(firstNameFunction){
+
     let RegExpFirstName = new RegExp("^[a-zA-Z -]{2,}$", "g");
 
-    if (RegExpFirstName.test(firstNameFunction.value)){
+    if (RegExpFirstName.test(firstName.value)){
         firstNameErrorMsg.innerHTML = "Format de prénom valide";
         return true;
+            
     }else{
-        firstNameErrorMsg.innerHTML = "Format de prénom invalide";
-        return false;
-    }
-};
+        firstNameErrorMsg.innerHTML = "Format de prénom invalide"; 
+        return false;       
+    }  
+});
+
 
 
 //Formulaire - Nom de famille
 let lastName = document.querySelector("#lastName");
 let lastNameErrorMsg = document.querySelector("#lastNameErrorMsg");
 lastName.addEventListener("change", function(){
-    checkLastName(this);
-});
-let checkLastName = function(lastNameFunction){
+
     let RegExpLastName = new RegExp("^[a-zA-Z -]{2,}$", "g");
 
-    if (RegExpLastName.test(lastNameFunction.value)){
+    if (RegExpLastName.test(lastName.value)){
         lastNameErrorMsg.innerHTML = "Format de nom valide";
         return true;
     }else{
         lastNameErrorMsg.innerHTML = "Format de nom invalide";
         return false;
     }
-};
+});
 
 //Formulaire - Adresse
 let address = document.querySelector("#address");
 let addressErrorMsg = document.querySelector("#addressErrorMsg");
 address.addEventListener("change", function(){
-    checkAddress(this);
-});
-let checkAddress = function(addressFunction){
+
     let RegExpAddress = new RegExp("^[a-zA-Z0-9 -]+$", "g");
 
-    if (RegExpAddress.test(addressFunction.value)){
+    if (RegExpAddress.test(address.value)){
         addressErrorMsg.innerHTML = "Format d'adresse valide";
         return true;
     }else{
         addressErrorMsg.innerHTML = "Format d'adresse invalide";
         return false;
     }
-};
+});
 
 //Formulaire - Ville
 let city = document.querySelector("#city");
 let cityErrorMsg = document.querySelector("#cityErrorMsg");
 city.addEventListener("change", function(){
-    checkCity(this);
-});
-let checkCity = function(cityFunction){
+
     let RegExpCity = new RegExp("^[0-9a-zA-Z -]+$", "g");
 
-    if (RegExpCity.test(cityFunction.value)){
+    if (RegExpCity.test(city.value)){
         cityErrorMsg.innerHTML = "Format de ville valide";
         return true;
     }else{
         cityErrorMsg.innerHTML = "Format de ville invalide";
         return false;
     }
-};
+});
 
 //Formulaire - Email
 let email = document.querySelector("#email");
 let emailErrorMsg = document.querySelector("#emailErrorMsg");
 email.addEventListener("change", function(){
-    checkEmail(this);
-});
-let checkEmail = function(emailFunction){
+
     let RegExpEmail = new RegExp ("^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$", "g");
  
-    if (RegExpEmail.test(emailFunction.value)){
+    if (RegExpEmail.test(email.value)){
         emailErrorMsg.innerHTML = "Format de d'email valide";
         return true;
+
     }else{
         emailErrorMsg.innerHTML = "Format de d'email invalide";
         return false;
     }
-};
+});
 
 // Afficher le numéro de commande
 let buttonOrder = document.querySelector("#order");
 buttonOrder.addEventListener("click", function(e){
-    e.preventDefault()
 
     let firstName = document.querySelector("#firstName").value
     let lastName = document.querySelector("#lastName").value
@@ -305,7 +297,9 @@ fetch("http://localhost:3000/api/products/order", {
 
       .catch(function(err) {
         console.log("Erreur")
+        alert("Veuillez compléter le formulaire")
     }) 
+
 
 });
 
